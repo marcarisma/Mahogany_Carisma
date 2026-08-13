f4ae78c7435264b7d6f00472dbf73188076350db
marcarisma <marcarisma31@src.pshs.edu.ph>
2026-08-13 09:16:30 +0800

    Add files via upload

---
+++ q1_8Mahogany_FA1_Carisma.py
@@ -0,0 +1,27 @@
+import math
+
+#ask the user to enter the coordinates of two points
+
+x1 = float(input("enter your x1 "))
+y1 = float(input("enter your x2 "))
+x2 = float(input("enter your y1 "))
+y2 = float(input("enter your y2 "))
+
+#subtract the coordinates
+subtract_x = (x2 - x1)
+subtract_y = (y2 - y1)
+
+
+#powers both the coordinates
+power_x = pow(subtract_x,2)
+power_y = pow(subtract_y,2)
+
+
+#adds the two coordinates
+addition = power_x + power_y
+
+#it square root the coordinate
+x= math.sqrt = (addition)
+
+#prints to ge the distance
+print ("the distance between the two point is" ,x)
